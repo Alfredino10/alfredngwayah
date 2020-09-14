10b555f47fcde9d97db89a89f4bc51e2296e3e16
alfredino10 <an017394@gmail.com>
2020-09-14 14:38:37 +0000

    Basic editing

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
+const sources = [
+    'images/alfred.jpg',
+    'images/monrovia.jpg'
+]
+
+var currentpic = 0;
+
+
 window.onload = function inIt() {
     let button = document.querySelector('#navbtn');
     let navBar = document.querySelector('.site-nav ul');
-   
+    
     button.addEventListener('click', function displayNav(evt) {
         navBar.classList.toggle('active');
     });
+
+
+}
+
+function loadNextpics() {
+    mypics.src = sources[currentpic % sources.length];
+    currentpic++;
+}
+function loadPreviouspics() {
+    mypics.src = sources[currentpic % sources.length];
+    currentpic--;
 }
